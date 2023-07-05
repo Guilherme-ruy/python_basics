a3107d85d0680bab13a954805a130b6e2cd2032f
Guilherme-ruy <guilherme.ruy@outlook.com>
2023-07-05 18:26:49 -0300

    Initial commit

---
+++ 02.07.23/comeco.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+print ("Hello World")
+print (10 % 3)
+
+x = 15
+y = 15
+
+x == y
+
+
+if x > y:
+    print ("X é maior que Y")
+
+elif x == y:
+    print ("São iguais")
+
+else:
+    print ("Y é maior que X")
