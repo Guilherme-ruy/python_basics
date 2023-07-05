e627936aa79f1c1dc03a9e34b6f3ec26a74aa2bd
Guilherme Ruy <138726885+Guilherme-ruy@users.noreply.github.com>
2023-07-05 20:25:13 -0300

    Update concatenar.py

--- 05.07.23/concatenar.py
+++ 05.07.23/concatenar.py
@@ -3,3 +3,11 @@ b = "Ruy"
 
 conc = a +" " + b
 print(conc)
+
+#--------
+v1 = "teste de"
+v2 = "testes"
+v3 = "python"
+conc2 = v1 + v2 + v3
+conc2_separada = "-".join(conc2 + v3)
+print (conc2_separada)
