9ca61542ecfdc65a719cb0deed65e04891c12357
Ruy <guilherme.ruy@outlook.com>
2024-02-28 12:26:06 -0300

    Subindo the_not

---
+++ basic exercises/the_not.py
@@ -0,0 +1,28 @@
+
+#invertendo o resultado de uma condição
+#reversing the result of a condition
+
+#1
+true_or_false = not 1000 > 1500
+print(true_or_false)
+
+#2
+true_or_false = not 1500 > 1500
+print(true_or_false)
+
+#3
+true_or_false = not 1500 == 1500
+print(true_or_false)
+
+#4
+true_or_false = not 1600 > 1500
+print(true_or_false)
+
+#5
+true_or_false = not 1000 < 1500
+print(true_or_false)
+
+#6
+true_or_false = not 1600 < 1500
+print(true_or_false)
+
